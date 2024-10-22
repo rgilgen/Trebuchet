04076f5559c3ff0592f796847cbcdfd28e43e674
nickdhima <ndimas.tech@gmail.com>
2024-10-22 12:35:19 +0200

    package private Methoden

--- src/main/java/ch/css/kata/Trebuchet.java
+++ src/main/java/ch/css/kata/Trebuchet.java
@@ -10,7 +10,12 @@ import java.util.stream.Collectors;
 
 public class Trebuchet {
 
-    public List<String> getStringFromFile(String path) throws IOException {
+    public int getCalibrationValuesFromFromFile(String input) throws IOException {
+        List<String> listOfStrings = getStringFromFile(input);
+        return listOfStrings.stream().map(this::getCalibrationValue).reduce(Integer::sum).orElseThrow();
+    }
+
+    List<String> getStringFromFile(String path) throws IOException {
         File file = new File(path);
         InputStream is = Files.newInputStream(file.toPath());
         Reader reader = new InputStreamReader(is);
@@ -19,25 +24,19 @@ public class Trebuchet {
         }
     }
 
-    public int getCalibrationValue(String input) {
+    int getCalibrationValue(String input) {
         Pattern pattern = Pattern.compile("(\\d).*(\\d)");
         Matcher matcher = pattern.matcher(input);
 
         String firstAndLastDigit;
         if (matcher.find()) {
             firstAndLastDigit = matcher.group(1) + matcher.group(2);
-        }
-        else{
+        } else {
             String digit = Pattern.compile("\\d").matcher(input).results()
                     .map(MatchResult::group)
                     .collect(Collectors.joining());
             firstAndLastDigit = digit + digit;
         }
         return Integer.parseInt(firstAndLastDigit);
-    }
-
-    public int getCalibrationValuesFromFromFile(String input) throws IOException {
-        List<String> listOfStrings = getStringFromFile(input);
-        return listOfStrings.stream().map(this::getCalibrationValue).reduce(Integer::sum).orElseThrow();
     }
 }
