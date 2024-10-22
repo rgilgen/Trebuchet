d1dc59ccd2fca197b45f9433fce4323840f2da4d
nickdhima <ndimas.tech@gmail.com>
2024-10-22 10:47:13 +0200

    Refactor with reducer , Added getCalibrationValueForOneNumber

--- src/main/java/ch/css/kata/Trebuchet.java
+++ src/main/java/ch/css/kata/Trebuchet.java
@@ -8,11 +8,16 @@ public class Trebuchet {
     public int getCalibrationValue(String input) {
         Pattern pattern = Pattern.compile("\\d+");
 
-        String numbers = pattern.matcher(input).results()
-                .map(MatchResult::group)
-                .reduce((first, second) -> second) // Get the last match
-                .map(last -> pattern.matcher(input).results().findFirst().map(first -> first.group() + last).orElse(last))
-                .orElse("");
+        String numbers = pattern.matcher(input).results().map(MatchResult::group).reduce((first, second) -> second) // Get the last match
+                .map(last -> appendFirstToLast(input, last, pattern)).orElse("");
         return Integer.parseInt(numbers);
+    }
+
+    private static String appendFirstToLast(String input, String last, Pattern pattern) {
+        return pattern.matcher(input)
+                .results()
+                .findFirst()
+                .map(first -> first.group() + last)
+                .orElse(last);
     }
 }
