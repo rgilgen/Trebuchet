6f2bb3b6bf93de1a5f419e7f9f07d2d74decaedb
nickdhima <ndimas.tech@gmail.com>
2024-10-22 10:03:35 +0200

    Initial implementation

--- src/main/java/ch/css/kata/Trebuchet.java
+++ src/main/java/ch/css/kata/Trebuchet.java
@@ -1,7 +1,21 @@
 package ch.css.kata;
 
 public class Trebuchet {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public int getCalibrationValue(String input) {
+        boolean isFirstFound = false;
+        int firstDigit = 0;
+        int lastDigit = 0;
+
+        for (Character c : input.toCharArray()) {
+            if (Character.isDigit(c)) {
+                if (!isFirstFound) {
+                    isFirstFound = true;
+                    firstDigit = Character.getNumericValue(c) * 10;
+                }
+                lastDigit = Character.getNumericValue(c);
+            }
+        }
+        return firstDigit + lastDigit;
     }
 }
---
+++ src/test/java/ch/css/kata/TrebuchetTest.java
@@ -0,0 +1,20 @@
+package ch.css.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrebuchetTest {
+
+    private final Trebuchet testee = new Trebuchet();
+
+    @Test
+    void getCalibrationValue() {
+        String input = "1abc2";
+        int expected = 12;
+
+        int actual = testee.getCalibrationValue(input);
+
+        assertEquals(expected, actual);
+    }
+}
